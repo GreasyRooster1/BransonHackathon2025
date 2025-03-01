41a0492b439be7cc5d1a5cabb0119dd5867c8cce
GreasyRooster <greasyRooster1@gmail.com>
2025-03-01 11:26:24 -0800

    blurs and refactor

--- src/js/blurs.js
+++ src/js/blurs.js
@@ -1,3 +1,13 @@
 function createBlurs(){
-    
+    for(let i=0;i<10;i++){
+        let el = createBlur();
+        document.querySelector(".bg-blurs").appendChild(el);
+    }
+}
+function createBlur(){
+    let el = document.createElement("div");
+    el.className = "blur"
+    el.style.top = (Math.random()*100)+"vh";
+    el.style.left = (Math.random()*100)+"vw";
+    return el
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,5 @@
-var apikey = "d3f0498fac234f7891405d986e872c52"
-
-async function get(url){
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+function init(){
+    createBlurs();
 }
 
-async function getRouteData(locations){
-    let transportation_mode = "drive"
-    let location_list = locations.join("|")
-    var response = await get(`https://api.geoapify.com/v1/routing?waypoints=${location_list}&mode=${transportation_mode}&apiKey=${apikey}`)
-    var info = response.features[0].properties
-    let data = {
-        distance: info.distance,
-        time: info.time,
-    }
-    return data
-}
-
-
-var locations = ["37.970187520374125,-122.52218960842936","37.95609038562469,-122.50912077728964"]
-async function test(){
-    var data = await getRouteData(locations)
-    console.log(data)
-}
-
-test()
+window.onload = init;
