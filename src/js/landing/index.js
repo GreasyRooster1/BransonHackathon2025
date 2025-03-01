ea6567d0a20a6bae880f027fe22a7dcf19c05d8a
GreasyRooster <greasyRooster1@gmail.com>
2025-03-01 15:22:04 -0800

    landing

--- src/js/landing/index.js
+++ src/js/landing/index.js
@@ -1,5 +1,29 @@
+let responses = []
+let questions = ["Do you use an EV?","Do you carpool regularly?","Do you have LED bulbs","Are you willing to imporove?"]
+let currentQuestion =0;
 function init(){
     createBlurs();
+    initEvents();
+}
+
+function initEvents(){
+    document.querySelector(".yes").addEventListener("click",()=>{
+        responses.push("yes")
+        nextQuestion()
+    })
+    document.querySelector(".no").addEventListener("click",()=>{
+        responses.push("no")
+        nextQuestion()
+    })
+}
+
+function nextQuestion(){
+    currentQuestion++;
+    if(currentQuestion==questions.length){
+        window.location.href = "./index.html";
+        return;
+    }
+    document.querySelector(".sub-title").innerHTML = questions[currentQuestion]
 }
 
 window.onload = init;
