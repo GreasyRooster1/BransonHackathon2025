739c4a2e89623bbbe32bb9f84a4551aa4e03b0af
kisvg <kaisavage@gmail.com>
2025-03-01 14:25:30 -0800

    are

--- src/js/summary.js
+++ src/js/summary.js
@@ -3,6 +3,10 @@ function setupSummaryEvents(){
         document.querySelector(".submit-button").classList.remove("active");
         document.querySelector(".summary").classList.add("active");
         calculateTotalEmmissions();
+        let score = 5
+        let emmissions = 5
+        document.querySelector(".data").innerHTML = score
+        document.getElementById("emmissions-data").innerHTML = emmissions
     });
 }
 
