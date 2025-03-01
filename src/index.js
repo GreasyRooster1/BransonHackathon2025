6320f3174c8c7de317def9fd21eaf67a40fc93d1
GreasyRooster <greasyRooster1@gmail.com>
2025-03-01 11:02:15 -0800

    glass

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-function getRouteData()
+var apikey = "d3f0498fac234f7891405d986e872c52"
+
+async function get(url){
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+async function getRouteData(locations){
+    var location_list = ""
+    locations.forEach(location => {
+        location_list += location + "|"
+    });
+    var data = await get(`ttps://api.geoapify.com/v1/routing?waypoints=${location_list}&mode=${transportation_mode}&apiKey=${apikey}`)
+    return data
+}
 
 
-getRouteData()
+var locations = ["37.970187520374125,-122.52218960842936","37.95609038562469,-122.50912077728964"]
+
+var data = await getRouteData(locations)
+console.log(data)
