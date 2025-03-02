36bf3470d8764e7699d540112114cb0ce3a01b87
GreasyRooster <greasyRooster1@gmail.com>
2025-03-01 18:26:39 -0800

    completed

--- src/js/index/missions.js
+++ src/js/index/missions.js
@@ -5,8 +5,10 @@ function addMissionListeners(){
     })
     document.getElementById("bike").addEventListener("click", ()=>{
         changeScore(30);
+        document.getElementById("bike").innerHTML = "Completed!"
     })
     document.getElementById("bottle").addEventListener("click", ()=>{
         changeScore(20);
+        document.getElementById("bottle").innerHTML = "Completed!"
     })
 }
