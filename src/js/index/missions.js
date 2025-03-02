74e4cdd5476257cb14be95df4f96288b9c075da1
GreasyRooster <greasyRooster1@gmail.com>
2025-03-01 17:44:12 -0800

    sfsdfs

--- src/js/index/missions.js
+++ src/js/index/missions.js
@@ -1,12 +1,12 @@
 function addMissionListeners(){
     document.getElementById("plant").addEventListener("click", ()=>{
+        document.getElementById("plant-bar").value += 1;
         changeScore(5);
     })
     document.getElementById("bike").addEventListener("click", ()=>{
         changeScore(30);
     })
     document.getElementById("bottle").addEventListener("click", ()=>{
-        document.getElementById("plant-bar").value += 1;
         changeScore(20);
     })
 }
